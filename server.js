bb58764a7611e56966682e05740265458e105049
joelepore <giuseppejoelepore@gmail.com>
2024-12-17 15:38:36 +0100

    feat(step 1): add root endpoint

--- server.js
+++ server.js
@@ -1,1 +1,9 @@
-console.log('server');
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+  res.send(`<h1>Server del mio blog</h1>`)
+})
+
+app.listen(port, () => console.log(`Listening on port ${port}`));
