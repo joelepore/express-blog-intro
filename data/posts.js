a56cd11180e4d8c0cfefff969b23eacabf88822e
joelepore <giuseppejoelepore@gmail.com>
2024-12-17 15:50:38 +0100

    feat(step 2): create posts array

---
+++ data/posts.js
@@ -0,0 +1,34 @@
+const posts = [
+  {
+    titolo: "Ciambellone",
+    contenuto: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean venenatis ipsum at nisi placerat, at tincidunt nunc fermentum. Integer ac purus in dui faucibus varius.",
+    immagine: "images/ciambellone.jpeg",
+    tags: ["dolci", "colazione", "ciambella"]
+  },
+  {
+    titolo: "Cracker Barbabietola",
+    contenuto: "Curabitur vitae nisi a felis feugiat viverra. Ut varius orci at velit scelerisque, eu tincidunt libero ultricies. Aliquam erat volutpat. Cras et urna sit amet dolor efficitur vestibulum.",
+    immagine: "images/cracker_barbabietola.jpeg",
+    tags: ["snack", "barbabietola", "salato"]
+  },
+  {
+    titolo: "Pane Fritto Dolce",
+    contenuto: "Sed malesuada, augue eget vehicula dapibus, eros est mollis lectus, nec bibendum lectus metus sed risus. Morbi eget urna sed ipsum pharetra auctor. Vivamus feugiat suscipit neque.",
+    immagine: "images/pane_fritto_dolce.jpeg",
+    tags: ["dolci", "fritto", "pane"]
+  },
+  {
+    titolo: "Pasta Barbabietola",
+    contenuto: "Suspendisse potenti. Curabitur sit amet dolor at velit convallis elementum. Duis luctus euismod risus, sit amet tempor est placerat sit amet. Ut ac lectus leo. Fusce vehicula erat ut magna suscipit, a scelerisque felis auctor.",
+    immagine: "images/pasta_barbabietola.jpeg",
+    tags: ["pasta", "barbabietola", "vegetariano"]
+  },
+  {
+    titolo: "Torta Paesana",
+    contenuto: "Integer maximus, nulla in mollis ultricies, ante erat volutpat purus, non gravida nunc turpis a elit. Integer tincidunt vel ante nec malesuada. Nam et velit sit amet nunc cursus pharetra.",
+    immagine: "images/torta_paesana.jpeg",
+    tags: ["torta", "dolci", "tradizionale"]
+  }
+];
+
+module.exports = posts;
