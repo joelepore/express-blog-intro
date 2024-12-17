6be7b17a4324d02020aba500b8d255208b0d369c
joelepore <giuseppejoelepore@gmail.com>
2024-12-17 15:55:39 +0100

    feat(step 3): add /bacheca endpoint

--- server.js
+++ server.js
@@ -1,9 +1,19 @@
+// Express
 const express = require('express');
 const app = express();
 const port = 3000;
+// Data
+const posts = require('./data/posts');
 
 app.get('/', (req, res) => {
   res.send(`<h1>Server del mio blog</h1>`)
+})
+
+app.get('/bacheca', (req, res) => {
+  res.json({
+    posts,
+    totalPosts: posts.length
+  });
 })
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
